d63da35069171099edbb39b74612585dd3cc5db4
Sorn <maksumic@protonmail.com>
2026-08-05 12:04:50 +0200

    Makes '/about' the homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sorn.dev',
+  redirects: {
+    '/about': '/'
+  },
   vite: {
     plugins: [tailwindcss()]
   }
